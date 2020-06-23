e8b39cd8607a5264a08713b4d200f52d1c5e5956
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-06-23 18:57:34 +0200

    add build

---
+++ build/precache-manifest.67bbb4dbaa0422f19721bd7024b7f39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588e63cf45475431a10a2efd4e4006ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8e1541eb85cfaf4c876",
+    "url": "./static/css/main.8417ae2c.chunk.css"
+  },
+  {
+    "revision": "ad3688774497e3a83634",
+    "url": "./static/js/2.632883e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.632883e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e1541eb85cfaf4c876",
+    "url": "./static/js/main.2498521e.chunk.js"
+  },
+  {
+    "revision": "ae733c3167a6a02d3f38",
+    "url": "./static/js/runtime-main.e368028e.js"
+  }
+]);
