9f033aad00cf1af3e55d8d5ad9f7af1095187a18
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-06-23 21:42:41 +0200

    update websocket-extensions

---
+++ build/precache-manifest.04dd3ab74670ab6591760b928e9d1f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cc3e9aaaba9e4d47bcf692cf753e23",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7299607704af95094f2c",
+    "url": "./static/css/main.143821ba.chunk.css"
+  },
+  {
+    "revision": "e426f5af75d607ae0867",
+    "url": "./static/js/2.11248fe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.11248fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7299607704af95094f2c",
+    "url": "./static/js/main.85f4c9c5.chunk.js"
+  },
+  {
+    "revision": "c9dd47137c9d0988e204",
+    "url": "./static/js/runtime-main.d64bcbb1.js"
+  }
+]);
